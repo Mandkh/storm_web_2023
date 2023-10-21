fdfb584e41c96c8a0fb5ca6a1c5549a79cc2820c
Storm-4 <storm-4@Admin-4s-iMac.local>
2023-10-21 13:05:55 +0800

    lesson_day_18

---
+++ lesson_day_18/js/script_if_else.js
@@ -0,0 +1,4 @@
+let workAge = 5
+if (workAge > 15) {
+    console.log('You can work');
+}
---
+++ lesson_day_18/js/scripts.js
@@ -0,0 +1,7 @@
+console.log('Lesson Day 18 - JS Loops');
+// FOR loop
+for (var i = 1; i <=10; i++) {
+    console.log('i = ' + i + ' hello');
+}
+
+console.log('after for loop');
