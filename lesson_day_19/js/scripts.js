f1a949b83d2acb4c60da47bc6a90081d62e39c1f
Storm-4 <storm-4@Admin-4s-iMac.local>
2023-10-24 10:38:37 +0800

    lesson_day_19

---
+++ lesson_day_19/js/scripts.js
@@ -0,0 +1,42 @@
+console.log('lesson_day_19 - Control structures exercises');
+
+// variables
+// number variables
+let too = 10;
+let too2 = 20;
+let too3 = 30;
+
+// string variables
+let ner = 'Mede';
+let ner2 = 'Naraa';
+let ner3 = 'Bilguun';
+
+// boolean variables
+let unen = 'true';
+let hudal = 'false';
+
+let isMongolian = true;
+let hasMongolianPassword = false;
+
+// if statement
+if (isMongolian) {
+    console.log('You are Mongolian.');
+}
+
+// if else statement
+if (isMongolian) {
+    console.log('You are Mongolian');
+} else if (isMongolian && hasMongolianPassword) {
+    console.log('You are Mongolian');
+}
+else {
+    console.log('You are not Mongolian.');
+}
+
+// loops
+// for loop
+
+for (let i = 0; i < 10; i++ ) {
+    console.log(i);
+    i++;
+}
